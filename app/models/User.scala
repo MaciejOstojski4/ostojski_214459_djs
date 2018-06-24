ebcdd4190f7158a1f99217d24d82e8f1451f8d78
kapjl-22 <kapjl-22@sr.dmcs.pl>
2018-06-24 10:23:41 +0200

    Add service layer

---
+++ app/models/User.scala
@@ -0,0 +1,47 @@
+package models
+
+import be.objectify.deadbolt.scala.models.Subject
+import play.api.data.Form
+import play.api.data.Forms._
+
+/**
+  *
+  * @author Steve Chaloner (steve@objectify.be)
+  */
+case class User(val id: Long, firstName: String, lastName: String, email: String, password: String) extends Subject {
+  override def roles: List[SecurityRole] =
+    List(SecurityRole("foo"),
+      SecurityRole("bar"))
+
+  override def permissions: List[UserPermission] =
+    List(UserPermission("printers.edit"))
+
+  override def identifier: String = id.toString
+}
+
+case class RegistrationFormData(firstName: String, lastName: String, email: String, password: String)
+
+object RegistrationForm {
+
+  val form = Form(
+    mapping(
+      "firstName" -> nonEmptyText,
+      "lastName" -> nonEmptyText,
+      "email" -> email,
+      "password" -> nonEmptyText
+    )(RegistrationFormData.apply)(RegistrationFormData.unapply)
+  )
+}
+
+case class LoginFormData(email: String, password: String)
+
+object LoginForm {
+  val form = Form(
+    mapping(
+      "email" -> email,
+      "password" -> nonEmptyText
+    )(LoginFormData.apply)(LoginFormData.unapply)
+  )
+}
+
+
